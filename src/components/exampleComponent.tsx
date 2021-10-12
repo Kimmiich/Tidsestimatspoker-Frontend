507ad16990dffc18958be73276c53991a59c83b9
loveefraimsson <loveefraimsson@gmail.com>
2021-10-12 13:21:52 +0200

    Added example-component

---
+++ src/components/exampleComponent.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+class exampleComponent extends React.Component {
+    render() {
+
+        return <h1>ExampleComponent</h1>
+    }
+}
+
+
+export default exampleComponent;
