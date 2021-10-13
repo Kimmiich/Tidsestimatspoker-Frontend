9bc9ba52d469d54be8ebcef717066c8d170f9967
Emil Nilsson <52598265+EmilNilsson12@users.noreply.github.com>
2021-10-13 09:41:02 +0200

    Prettier formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,20 @@
-
 import React, { useState } from 'react';
-
 
 import StartPage from './components/Startpage';
 
-
 function App() {
+	const [userName, setUserName] = useState('');
 
-  const [userName, setUserName] = useState("");
+	let saveUser = (userName: string) => {
+		console.log('saveUser()', userName);
+		setUserName(userName);
+	};
 
-  let saveUser = (userName: string) => {
-    console.log("saveUser()", userName);
-    setUserName(userName);
-  } 
-
-  return (
-    <div className="App">
-
-
-        <StartPage getUser={saveUser} />
-        
-
-    </div>
-  );
+	return (
+		<div className='App'>
+			<StartPage getUser={saveUser} />
+		</div>
+	);
 }
 
 export default App;
