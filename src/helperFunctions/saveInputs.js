0b00d535478427ce21032c91b718e84ab471a22d
Kimmiich <kimmielundgren@live.se>
2021-10-15 10:24:44 +0200

    Nu tror jag den formaterade om

--- src/helperFunctions/saveInputs.js
+++ src/helperFunctions/saveInputs.js
@@ -2,18 +2,17 @@ const API_URL = 'https://timeestimator.herokuapp.com/';
 
 //Accepts a object with inputvalue and userName/id?
 const saveInput = (obj) => {
-  console.log(obj);
-  fetch(API_URL, {
-    method: 'post',
-    headers: { 'Content-type': 'application/json' },
-    body: JSON.stringify(obj),
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      console.log('data', data);
+    console.log(obj);
+    fetch(API_URL, {
+        method: 'post',
+        headers: { 'Content-type': 'application/json' },
+        body: JSON.stringify(obj),
     })
-    .catch((err) => console.log(err));
+        .then((res) => res.json())
+        .then((data) => {
+            console.log('data', data);
+        })
+        .catch((err) => console.log(err));
 };
-
 
 export default saveInput;
