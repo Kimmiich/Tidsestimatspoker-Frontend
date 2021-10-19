ac44e92c828ec77c44d06d00033441f64cada3d9
Kimmiich <kimmielundgren@live.se>
2021-10-19 22:20:14 +0200

    Started on styling on startpage, gave all elements classnames

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
     };
 
     return (
-        <div className="App">
+        <>
             {userName ? (
                 <>
                     <button onClick={() => saveUser('')}>Logga ut</button>
@@ -21,10 +21,9 @@ function App() {
             ) : (
                 <>
                     <StartPage getUser={saveUser} />
-                    <p>Vänligen logga in</p>
                 </>
             )}
-        </div>
+        </>
     );
 }
 
--- src/components/Startpage/Startpage.tsx
+++ src/components/Startpage/Startpage.tsx
@@ -1,49 +1,55 @@
-
-import React from "react";
+import React from 'react';
+import './Startpage.css';
 
 interface Props {
-    getUser(userName: string): void
+    getUser(userName: string): void;
 }
 
 interface State {
-    userName: string
+    userName: string;
 }
-
 
 class StartPage extends React.Component<Props, State> {
-    
     state = {
-        userName: "",
-    }
+        userName: '',
+    };
 
     render() {
-
         let onChange = (evt: any) => {
-            console.log("onChange", evt.target.value);
-            this.setState({userName: evt.target.value})            
-        }
+            console.log('onChange', evt.target.value);
+            this.setState({ userName: evt.target.value });
+        };
 
         let onClick = () => {
-            this.props.getUser(this.state.userName)
-        } 
-        
+            this.props.getUser(this.state.userName);
+        };
+
         return (
             <div className="startpage-container">
-                <select name="userNames" id="userNames" onChange={onChange}>
-                    <option></option>
-                    <option value="Kimmie">Kimmie</option>
-                    <option value="Stina">Stina</option>
-                    <option value="Emil">Emil</option>
-                    <option value="Love">Love</option>
-                    <option value="Björn">Björn</option>
-                </select>
-                <button onClick={onClick}>Välj användare</button>
+                <main className="startpage-box">
+                    <section className="startpage-sign-in">
+                        <h1>TIDSESTIMATOR</h1>
+                        <select
+                            name="userNames"
+                            id="userNames"
+                            onChange={onChange}
+                        >
+                            <option value="">Välj användare</option>
+                            <option value="Kimmie">Kimmie</option>
+                            <option value="Stina">Stina</option>
+                            <option value="Emil">Emil</option>
+                            <option value="Love">Love</option>
+                            <option value="Björn">Björn</option>
+                        </select>
+                        <button onClick={onClick}>Logga in</button>
+                    </section>
+                    <section className="footer">
+                        <p>Välj en användare för att logga in</p>
+                    </section>
+                </main>
             </div>
-        ) 
-
+        );
     }
-
 }
-
 
 export default StartPage;
