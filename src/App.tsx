47a629159baa3da0f0ba2bb358b26d37df3ddcb6
Kimmiich <kimmielundgren@live.se>
2021-10-19 21:11:13 +0200

    Added map and file for startpage and startpage.css, Also removed icon in manifest

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 import SignedIn from './components/SignedIn';
-import StartPage from './components/Startpage';
+import StartPage from './components/Startpage/Startpage';
 
 function App() {
     const [userName, setUserName] = useState('');
