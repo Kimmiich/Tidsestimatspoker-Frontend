d8b754480ef9725c8c5c48cf3a6da0bc2bb1ea00
Kimmiich <kimmielundgren@live.se>
2021-10-19 20:25:06 +0200

    TotalSummary

--- src/helperFunctions/calculations.js
+++ src/helperFunctions/calculations.js
@@ -1,21 +1,36 @@
 // Det mittersta värdet av en sorterad lista
 export const median = (array) => {
-  const mid = Math.floor(array.length / 2),
-    nums = [...array].sort((a, b) => a - b);
-  return array.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2
-}
+    const mid = Math.floor(array.length / 2),
+        nums = [...array].sort((a, b) => a - b);
+    return array.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2;
+};
 
 // Medelvärdet
 export const average = (array) => {
-  return array.reduce((a, b) => a + b, 0) / array.length
-}
+    return array.reduce((a, b) => a + b, 0) / array.length;
+};
 
 //Maxvärdet
 export const max = (array) => {
-  return Math.max(...array)
-}
+    return Math.max(...array);
+};
 
 //Minvärdet
 export const min = (array) => {
-  return Math.min(...array)
-}
+    return Math.min(...array);
+};
+
+//Funktion som adderar alla medelvärden
+export const totalTime = (array) => {
+    let total = 0;
+    for (let number in array) {
+        total += array[number];
+    }
+    return total;
+};
+
+//Funktion som räknar ut diff
+export const diffTotal = (average, real) => {
+    let diff = real - average;
+    return diff;
+};
